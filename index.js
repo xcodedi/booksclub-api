14ccd520c237880d4038ae37a18fd084c15b0ac6
Diego Meira <di.meira@hotmail.com>
2024-06-13 20:36:00 -0300

    Add users login

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config(); // Executa a função config para carregar as variáveis de ambiente
+
 import express from "express";
 import db from "./src/models";
 import usercontroller from "./src/controllers/user";
+
 const app = express();
 
 app.use(express.json());
 app.post("/", usercontroller.create);
+app.post("/login", usercontroller.login);
+
 const PORT = 4000;
 app.listen(PORT, async () => {
   try {
